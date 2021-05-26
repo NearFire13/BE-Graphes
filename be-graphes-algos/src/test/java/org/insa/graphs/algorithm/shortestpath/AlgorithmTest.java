bcf6f8e12ce0b992089681dceae9f35fd065d12e
Benjamin ABONNEAU <abonneau.benjamin@laposte.net>
2021-05-26 19:21:51 +0200

    JUnits Test A*.

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/AlgorithmTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/AlgorithmTest.java
@@ -21,7 +21,7 @@ import org.insa.graphs.model.io.PathReader;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DijkstraAlgorithmTest {
+public class AlgorithmTest {
 
     // Graphs use for tests
     private static Graph graphCarreDense, graphHauteGaronne;
@@ -32,19 +32,19 @@ public class DijkstraAlgorithmTest {
     // Some maps to read
     private static GraphReader readerCarreDense, readerHauteGaronne;
     
-    // Somes nodes
-    //private static Node origin, destination;
-    
     protected static ShortestPathData dataBikiniInfeasibleLengthMode, dataBikiniLengthMode, dataCarreDenseLengthMode, dataCarreDenseTimeMode, randomDataCarreDenseLengthMode, randomDataCarreDenseLengthModeOnlyCars, randomDataCarreDenseTimeMode, randomDataCarreDenseTimeModeOnlyCars, randomDataCarreDenseTimeModePedestrian;
     
-    protected static ShortestPathSolution singleNodeDijkstraLengthMode, singleNodeDijkstraTimeMode, dijkstraBikiniLengthMode, dijkstraBikiniInfeasibleLengthMode;
+    protected static ShortestPathSolution singleNodeDijkstraLengthMode, singleNodeDijkstraTimeMode, singleNodeAStarLengthMode, singleNodeAStarTimeMode, dijkstraBikiniLengthMode, AStarBikiniLengthMode, dijkstraBikiniInfeasibleLengthMode, AStarBikiniInfeasibleLengthMode;
     
     protected static ShortestPathSolution[] randomDijkstraLengthMode, randomDijkstraLengthModeOnlyCars, randomDijkstraTimeMode, randomDijkstraTimeModeOnlyCars, randomDijkstraTimeModePedestrian,
-    										randomBellmanFordLengthMode, randomBellmanFordLengthModeOnlyCars, randomBellmanFordTimeMode, randomBellmanFordTimeModeOnlyCars, randomBellmanFordTimeModePedestrian;
+    										randomBellmanFordLengthMode, randomBellmanFordLengthModeOnlyCars, randomBellmanFordTimeMode, randomBellmanFordTimeModeOnlyCars, randomBellmanFordTimeModePedestrian,
+    										randomAStarLengthMode, randomAStarLengthModeOnlyCars, randomAStarTimeMode, randomAStarTimeModeOnlyCars, randomAStarTimeModePedestrian;
     
     protected static DijkstraAlgorithm dijkstraAlgorithm;
     
     protected static BellmanFordAlgorithm bellmanFordAlgorithm;
+    
+    protected static AStarAlgorithm AStarAlgorithm;
     
     private static PathReader readerBikini;
     
@@ -58,7 +58,7 @@ public class DijkstraAlgorithmTest {
         
         
         //Get CarreDense's Graph
-        mapCarreDense = mapDirectory + "carre-dense.mapgr";
+        mapCarreDense = mapDirectory + "carre.mapgr";
         readerCarreDense = new BinaryGraphReader(
         		new DataInputStream(new BufferedInputStream(new FileInputStream(mapCarreDense))));
         graphCarreDense = readerCarreDense.read();
@@ -75,13 +75,10 @@ public class DijkstraAlgorithmTest {
         		new DataInputStream(new BufferedInputStream(new FileInputStream(pathBikiniName))));
         pathBikini = readerBikini.readPath(graphHauteGaronne);
         
-        /*origin = graphMidiPyrenees.getNodes().get(10991);
-        destination = graphMidiPyrenees.getNodes().get(63104);*/
-        
         /*
-         * ----------------------------------------
-         * Single Node Test with Dijkstra Algorithm
-         * ----------------------------------------
+         * -----------------------------------------------
+         * Single Node Test with Dijkstra and A* Algorithm
+         * -----------------------------------------------
          */
         Node singleNode = graphCarreDense.getNodes().get(new Random().nextInt(graphCarreDense.getNodes().size()));
         singleNodePath = new Path(graphCarreDense, singleNode);
@@ -95,6 +92,15 @@ public class DijkstraAlgorithmTest {
         //Dijkstra Time Mode (2)
         dijkstraAlgorithm = new DijkstraAlgorithm(dataCarreDenseTimeMode);
         singleNodeDijkstraTimeMode = dijkstraAlgorithm.doRun();
+        
+        
+        //A* Length Mode (0)
+        AStarAlgorithm = new AStarAlgorithm(dataCarreDenseLengthMode);
+        singleNodeAStarLengthMode = AStarAlgorithm.doRun();
+        
+        //A* Time Mode (2)
+        AStarAlgorithm = new AStarAlgorithm(dataCarreDenseTimeMode);
+        singleNodeAStarTimeMode = AStarAlgorithm.doRun();
         
 
         /*
@@ -114,6 +120,12 @@ public class DijkstraAlgorithmTest {
         randomBellmanFordTimeMode = new ShortestPathSolution[50];
         randomBellmanFordTimeModeOnlyCars = new ShortestPathSolution[50];
         randomBellmanFordTimeModePedestrian = new ShortestPathSolution[50];
+        
+        randomAStarLengthMode = new ShortestPathSolution[50];
+        randomAStarLengthModeOnlyCars = new ShortestPathSolution[50];
+        randomAStarTimeMode = new ShortestPathSolution[50];
+        randomAStarTimeModeOnlyCars = new ShortestPathSolution[50];
+        randomAStarTimeModePedestrian = new ShortestPathSolution[50];
         
         for(int i = 0; i < 50; i++)
         {
@@ -169,6 +181,28 @@ public class DijkstraAlgorithmTest {
 			//BellmanFord Time Mode Pedestrian (4)
 			bellmanFordAlgorithm = new BellmanFordAlgorithm(randomDataCarreDenseTimeModePedestrian) ; 
 			randomBellmanFordTimeModePedestrian[i] = bellmanFordAlgorithm.doRun();
+			
+			
+			
+            //A* Length Mode (0)
+			AStarAlgorithm = new AStarAlgorithm(randomDataCarreDenseLengthMode) ; 
+			randomAStarLengthMode[i] = AStarAlgorithm.doRun();
+			
+			//A* Length Mode Only Cars (1)
+			AStarAlgorithm = new AStarAlgorithm(randomDataCarreDenseLengthModeOnlyCars) ; 
+			randomAStarLengthModeOnlyCars[i] = AStarAlgorithm.doRun();
+			
+			//A* Time Mode (2)
+			AStarAlgorithm = new AStarAlgorithm(randomDataCarreDenseTimeMode) ; 
+			randomAStarTimeMode[i] = AStarAlgorithm.doRun();
+			
+			//A* Time Mode Only Cars (3)
+			AStarAlgorithm = new AStarAlgorithm(randomDataCarreDenseTimeModeOnlyCars) ; 
+			randomAStarTimeModeOnlyCars[i] = AStarAlgorithm.doRun();
+			
+			//A* Time Mode Pedestrian (4)
+			AStarAlgorithm = new AStarAlgorithm(randomDataCarreDenseTimeModePedestrian) ; 
+			randomAStarTimeModePedestrian[i] = AStarAlgorithm.doRun();
         }
         
         /*
@@ -183,6 +217,10 @@ public class DijkstraAlgorithmTest {
 		//Dijkstra Bikini Infeasible Length Mode (0)
 		dijkstraAlgorithm = new DijkstraAlgorithm(dataBikiniInfeasibleLengthMode); 
 		dijkstraBikiniInfeasibleLengthMode = dijkstraAlgorithm.doRun(); 
+		
+		//A* Bikini Infeasible Length Mode (0)
+		AStarAlgorithm = new AStarAlgorithm(dataBikiniInfeasibleLengthMode); 
+		AStarBikiniInfeasibleLengthMode = AStarAlgorithm.doRun(); 
         
         /*
          * -------------------------------------
@@ -195,14 +233,20 @@ public class DijkstraAlgorithmTest {
         dijkstraAlgorithm = new DijkstraAlgorithm(dataBikiniLengthMode);
         dijkstraBikiniLengthMode = dijkstraAlgorithm.doRun();
         
+        //A* Bikini Length Mode (0)
+        AStarAlgorithm = new AStarAlgorithm(dataBikiniLengthMode);
+        AStarBikiniLengthMode = AStarAlgorithm.doRun();
+        
 
     }
     
     /*
-     * ----------------------------------------
-     * Single Node Test with Dijkstra Algorithm
-     * ----------------------------------------
+     * -----------------------------------------------
+     * Single Node Test with Dijkstra and A* Algorithm
+     * -----------------------------------------------
      */
+    
+    // Dijkstra Algorithm
     @Test
     public void singleNodePathDijkstraAlgorithmLengthMode() {
 		assertEquals(0, singleNodePath.getOrigin().compareTo(singleNodeDijkstraLengthMode.getPath().getOrigin()));
@@ -217,12 +261,29 @@ public class DijkstraAlgorithmTest {
 		assertTrue(singleNodeDijkstraTimeMode.getPath().isValid());
     }
     
+    // A* Algorithm
+    @Test
+    public void singleNodePathAStarAlgorithmLengthMode() {
+		assertEquals(0, singleNodePath.getOrigin().compareTo(singleNodeAStarLengthMode.getPath().getOrigin()));
+		assertTrue(Math.abs(singleNodePath.getLength() - singleNodeAStarLengthMode.getPath().getLength()) < 0.01);
+		assertTrue(singleNodeAStarLengthMode.getPath().isValid());
+    }
+    
+    @Test
+    public void singleNodePathAStarAlgorithmTimeMode() {
+		assertEquals(0, singleNodePath.getOrigin().compareTo(singleNodeAStarTimeMode.getPath().getOrigin()));
+		assertTrue(Math.abs(singleNodePath.getMinimumTravelTime() - singleNodeAStarTimeMode.getPath().getMinimumTravelTime()) < 0.01);
+		assertTrue(singleNodeAStarTimeMode.getPath().isValid());
+    }
+    
     
     /*
-     * ------------------------------------------
-     * Pair of Nodes Test with Dijkstra Algorithm
-     * ------------------------------------------
+     * ----------------------------------------------------
+     * Pair of Nodes Test with Dijkstra and AStar Algorithm
+     * ----------------------------------------------------
      */
+    
+    // Dijkstra Algorithm
     @Test
     public void randomPathDijkstraAlgorithmLengthMode() {
     	for(int i = 0; i < 50; i++)
@@ -273,26 +334,95 @@ public class DijkstraAlgorithmTest {
     	}
     }
     
+    // A* Algorithm
+    @Test
+    public void randomPathAStarAlgorithmLengthMode() {
+    	for(int i = 0; i < 50; i++)
+    	{
+			assertEquals(0, randomAStarLengthMode[i].getPath().getOrigin().compareTo(randomBellmanFordLengthMode[i].getPath().getOrigin()));
+			assertTrue(Math.abs(randomAStarLengthMode[i].getPath().getLength() - randomBellmanFordLengthMode[i].getPath().getLength()) < 0.01);
+			assertTrue(randomAStarLengthMode[i].getPath().isValid());
+    	}
+    }
+    
+    @Test
+    public void randomPathAStarAlgorithmLengthModeOnlyCars() {
+    	for(int i = 0; i < 50; i++)
+    	{
+			assertEquals(0, randomAStarLengthModeOnlyCars[i].getPath().getOrigin().compareTo(randomBellmanFordLengthModeOnlyCars[i].getPath().getOrigin()));
+			assertTrue(Math.abs(randomAStarLengthModeOnlyCars[i].getPath().getLength() - randomBellmanFordLengthModeOnlyCars[i].getPath().getLength()) < 0.01);
+			assertTrue(randomAStarLengthModeOnlyCars[i].getPath().isValid());
+    	}
+    }
+    
+    @Test
+    public void randomPathAStarAlgorithmTimeMode() {
+    	for(int i = 0; i < 50; i++)
+    	{
+			assertEquals(0, randomAStarTimeMode[i].getPath().getOrigin().compareTo(randomBellmanFordTimeMode[i].getPath().getOrigin()));
+			assertTrue(Math.abs(randomAStarTimeMode[i].getPath().getMinimumTravelTime() - randomBellmanFordTimeMode[i].getPath().getMinimumTravelTime()) < 0.01);
+			assertTrue(randomAStarTimeMode[i].getPath().isValid());
+    	}
+    }
+    
+    @Test
+    public void randomPathAStarAlgorithmTimeModeOnlyCars() {
+    	for(int i = 0; i < 50; i++)
+    	{
+			assertEquals(0, randomAStarTimeModeOnlyCars[i].getPath().getOrigin().compareTo(randomBellmanFordTimeModeOnlyCars[i].getPath().getOrigin()));
+			assertTrue(Math.abs(randomAStarTimeModeOnlyCars[i].getPath().getMinimumTravelTime() - randomBellmanFordTimeModeOnlyCars[i].getPath().getMinimumTravelTime()) < 0.01);
+			assertTrue(randomAStarTimeModeOnlyCars[i].getPath().isValid());
+    	}
+    }
+    
+    @Test
+    public void randomPathAStarAlgorithmTimeModePedestrian() {
+    	for(int i = 0; i < 50; i++)
+    	{
+			assertEquals(0, randomAStarTimeModePedestrian[i].getPath().getOrigin().compareTo(randomBellmanFordTimeModePedestrian[i].getPath().getOrigin()));
+			assertTrue(Math.abs(randomAStarTimeModePedestrian[i].getPath().getMinimumTravelTime() - randomBellmanFordTimeModePedestrian[i].getPath().getMinimumTravelTime()) < 0.01);
+			assertTrue(randomAStarTimeModePedestrian[i].getPath().isValid());
+    	}
+    }
+    
     /*
-     * -------------------------------------
-     * Infeasible Path Test with Dijkstra Algorithm
-     * -------------------------------------
+     * ---------------------------------------------------
+     * Infeasible Path Test with Dijkstra and A* Algorithm
+     * ---------------------------------------------------
      */
+    
+    //Dijkstra Algorithm
 	@Test 
 	public void infeasiblePathDijkstraAlgorithmLengthMode() {
 		assertEquals(dijkstraBikiniInfeasibleLengthMode.getStatus(), AbstractSolution.Status.INFEASIBLE);
+	}
+	
+	//A* Algorithm
+	@Test 
+	public void infeasiblePathAStarAlgorithmLengthMode() {
+		assertEquals(AStarBikiniInfeasibleLengthMode.getStatus(), AbstractSolution.Status.INFEASIBLE);
 	}
     
     
     /*
      * -------------------------------------
-     * Big Path Test with Dijkstra Algorithm
+     * Big Path Test with Dijkstra and A* Algorithm
      * -------------------------------------
      */
+	
+	//Dijkstra Algorithm
     @Test
     public void bigPathDijkstraAlgorithmLengthMode() {
 		assertEquals(0, dijkstraBikiniLengthMode.getPath().getOrigin().compareTo(pathBikini.getOrigin()));
 		assertTrue(Math.abs(dijkstraBikiniLengthMode.getPath().getLength() - pathBikini.getLength()) < 0.01);
 		assertTrue(dijkstraBikiniLengthMode.getPath().isValid());
+    }
+    
+    //A* Algorithm
+    @Test
+    public void bigPathAStarAlgorithmLengthMode() {
+		assertEquals(0, AStarBikiniLengthMode.getPath().getOrigin().compareTo(pathBikini.getOrigin()));
+		assertTrue(Math.abs(AStarBikiniLengthMode.getPath().getLength() - pathBikini.getLength()) < 0.01);
+		assertTrue(AStarBikiniLengthMode.getPath().isValid());
     }
 }
